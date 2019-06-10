b454259f90a77a8e540c84442cb87c0ab67c053d
Uwe Fuchs <info@uwefuchs.com>
2019-06-10 17:07:10 +0200

    mark selected hero in list via teavm-EL

--- src/main/java/com/uwefuchs/demo/teavm/tourofheroes/service/MockHeroesService.java
+++ src/main/java/com/uwefuchs/demo/teavm/tourofheroes/service/MockHeroesService.java
@@ -6,48 +6,48 @@ import java.util.Map;
 import com.uwefuchs.demo.teavm.tourofheroes.model.HeroDto;
 
 public class MockHeroesService {
-	private static int heroId = 10;
-	private static final Map<Integer, HeroDto> heroesMap;
+    private static int heroId = 10;
+    private static final Map<Integer, HeroDto> heroesMap;
 
-	static {
-		heroesMap = new LinkedHashMap<>();
+    static {
+        heroesMap = new LinkedHashMap<>();
 
-		int id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Mr. Nice"));
+        int id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Mr. Nice"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Narco"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Narco"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Bombasto"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Bombasto"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Celeritas"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Celeritas"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Magneta"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Magneta"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "RubberMan"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "RubberMan"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Dynama"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Dynama"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Dr IQ"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Dr IQ"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Magma"));
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Magma"));
 
-		id = getNextId();
-		heroesMap.put(id, new HeroDto(id, "Tornado"));
-	}
+        id = getNextId();
+        heroesMap.put(id, new HeroDto(id, "Tornado"));
+    }
 
-	private static int getNextId() {
-		return ++heroId;
-	}
+    private static int getNextId() {
+        return ++heroId;
+    }
 
-	public static Map<Integer, HeroDto> getAllHeroes() {
-		return heroesMap;
-	}
+    public static Map<Integer, HeroDto> getAllHeroes() {
+        return heroesMap;
+    }
 }
